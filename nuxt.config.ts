6f0c287f21fb5f0672b5d4fa1ab1cf16ad38ec28
ElianRemaggi <elian.remaggi@hpc.org.ar>
2025-07-10 14:44:20 -0300

    Add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from 'node:url';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
+  alias: {
+    '@': fileURLToPath(new URL('./', import.meta.url))
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
