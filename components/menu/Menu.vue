c086f6b8f39057e18bffc490f31938a42b78e597
ElianRemaggi <elian.remaggi@hpc.org.ar>
2025-07-10 15:14:32 -0300

    Menu component

---
+++ components/menu/Menu.vue
@@ -0,0 +1,9 @@
+<template>
+<header class="bg-white">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <NuxtLink to="/FizzBuzz" class="w-full flex justify-center">
+        <a href="#" class="text-sm/6 mx-20 font-semibold text-gray-900">Fizz Buzz</a>
+      </NuxtLink>
+    </nav>
+</header>
+</template>
