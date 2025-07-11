a2fc807ac2be5db337066ecd20c1e2529d971cdc
Elian Remaggi <elianremaggi@gmail.com>
2025-07-10 21:40:00 -0300

    STYLE ( Menu ) : Set bases of style page

--- components/menu/Menu.vue
+++ components/menu/Menu.vue
@@ -1,9 +1,12 @@
 <template>
-<header class="bg-white">
+  <header class="bg-gradient-to-r from-emerald-900 to-blue-900 shadow-[0_4px_12px_-2px_rgba(52,211,153,0.3)]">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <NuxtLink to="/FizzBuzz" class="w-full flex justify-center">
-        <a href="#" class="text-sm/6 mx-20 font-semibold text-gray-900">Fizz Buzz</a>
+
+      <NuxtLink to="/FizzBuzz" class="flex justify-center flex-1">
+        <a href="#"
+          class="text-sm/6 mx-20 font-semibold text-white hover:text-emerald-300 transition-colors duration-200">Fizz
+          Buzz</a>
       </NuxtLink>
     </nav>
-</header>
+  </header>
 </template>
