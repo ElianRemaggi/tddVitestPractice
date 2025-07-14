ac5c4101735be3dfaa298c6f9f4df64cbf78baf5
ElianRemaggi <elian.remaggi@hpc.org.ar>
2025-07-14 14:01:08 -0300

    REFACTOR ( menu ) : refactor template un iterate a menu item array

--- components/menu/Menu.vue
+++ components/menu/Menu.vue
@@ -1,18 +1,37 @@
+<script setup lang="ts">
+
+interface menuItem {
+  name: string,
+  url: string
+}
+
+const menuitem = ref<menuItem[]>([])
+
+menuitem.value = [
+  {
+    name: 'FizzBuzz',
+    url: '/FizzBuzz'
+  },
+  {
+    name: 'TDD',
+    url: '/TDD'
+  },
+  {
+    name: 'Vitest',
+    url: '/Vitest'
+  }
+]
+</script>
 <template>
   <header class="bg-gradient-to-r from-emerald-900 to-blue-900 shadow-[0_4px_12px_-2px_rgba(52,211,153,0.3)]">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
 
-      <NuxtLink to="/FizzBuzz"
-        class="flex flex-1 justify-center mx-20 text-sm font-semibold text-white hover:text-emerald-300 transition-colors duration-200">
-        Fizz Buzz
-      </NuxtLink>
-      <NuxtLink to="/TDD"
-        class="flex flex-1 justify-center mx-20 text-sm font-semibold text-white hover:text-emerald-300 transition-colors duration-200">
-        TDD Page
-      </NuxtLink>
-      <NuxtLink to="/Vitest"
-        class="flex flex-1 justify-center mx-20 text-sm font-semibold text-white hover:text-emerald-300 transition-colors duration-200">
-        Vitest Page
+      <NuxtLink v-for="item in menuitem" :key="item.url" 
+        :to="item.url"
+        class="flex flex-1 justify-center mx-20 text-sm font-semibold text-white hover:text-emerald-300
+        transition-colors duration-200"
+        >
+        {{ item.name }}
       </NuxtLink>
     </nav>
   </header>
