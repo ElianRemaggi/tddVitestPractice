4fc429156d28507ab58d1c4d009f0d078099ec59
elianremaggi <elianremaggi@gmail.com>
2025-07-24 19:26:42 +0000

    FEAT ( FizzBuzzSolve ) : Create page

---
+++ app/pages/FizzBuzzSolve.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { title } from 'process';
+
+
+</script>
+
+<template>
+  <div class="min-h-screen mt-20 text-white">
+
+    <DotBackground class="absolute top-0 left-0 w-full h-full" />
+
+  </div>
+
+</template>
+
+<style scoped></style>
