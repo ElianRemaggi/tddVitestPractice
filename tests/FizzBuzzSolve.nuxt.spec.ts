339c86b2c72bf7fabc332719adaaa61d58deef6b
elianremaggi <elianremaggi@gmail.com>
2025-07-28 18:32:55 +0000

    TEST ( FizzBuzz ) : Add 3 testi to component instance, execture type and executre return

--- tests/FizzBuzzSolve.nuxt.spec.ts
+++ tests/FizzBuzzSolve.nuxt.spec.ts
@@ -1,16 +1,23 @@
 import { describe, it, expect } from 'vitest'
-import { mountSuspended } from '@nuxt/test-utils/runtime' 
+import { mountSuspended } from '@nuxt/test-utils/runtime'
 import FizzBuzzSolve from '../app/pages/FizzBuzzSolve.vue'
 
-const wrapper = await mountSuspended(FizzBuzzSolve) 
 
 describe('FizzBuzzSolve.vue', () => {
     it('FizzBuzz should exist', async () => {
+        const wrapper = await mountSuspended(FizzBuzzSolve)
         expect(wrapper.exists()).toBe(true);
     })
 
     it('FizzBuzz.execute should be a function', async () => {
+        const wrapper = await mountSuspended(FizzBuzzSolve)
+        expect(wrapper.vm).toHaveProperty('execute');
         expect(wrapper.vm.execute).toBeInstanceOf(Function);
     })
 
+    it('FizzBuzz.execute should return string', async () => {
+        const wrapper = await mountSuspended(FizzBuzzSolve);
+        const executionResult = wrapper.vm.execute();
+        expect(executionResult).toBeInstanceOf('string');
+    })
 })
