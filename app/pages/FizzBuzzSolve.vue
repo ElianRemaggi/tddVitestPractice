c70b342e22ee3b21684077826afdca8f17058827
elianremaggi <elianremaggi@gmail.com>
2025-07-29 12:42:01 +0000

    FEAT ( FizzBuzzSolve ) : Full implement of exectue funciton

--- app/pages/FizzBuzzSolve.vue
+++ app/pages/FizzBuzzSolve.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 
 //Funcion que se llama execute ,
-const execute  = () : string => {
-  return "asd"
+const execute  = (number : number) : string => {
+  
+  if(number % 3 === 0 && number % 5 === 0){
+    return "FizzBuzz"
+  }
+  if(number % 3 === 0){
+    return "Fizz"
+  }
+  if(number % 5 === 0){
+    return "Buzz"
+  }
+
+  return number.toString()
 }
 
 defineExpose({ execute })
