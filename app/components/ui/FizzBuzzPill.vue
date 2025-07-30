05178caeafc1ccda11e34aaf09f0e31987d263a1
ElianRemaggi <elianremaggi@gmail.com>
2025-07-30 17:35:33 +0000

    FEAT ( FizzBuzzPill ) : Create Pill component to render solution

---
+++ app/components/ui/FizzBuzzPill.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+interface Props {
+  text: string;
+}
+
+const props = defineProps<Props>();
+
+const getColorByText = ( text : string ): string => {
+    switch(text){
+        case 'Fizz' : return 'bg-emerald-500';
+        case 'Buzz' : return 'bg-cyan-500';
+        case 'FizzBuzz' : return 'bg-purple-500';
+        default : return 'bg-gray-500'
+    }
+}
+
+defineExpose({getColorByText})
+
+</script>
+
+<template>
+    <div class="flex items-center justify-center h-screen rounded-3xl">
+        <span >{{text}}</span>
+    </div>
+</template>
+
+<style lang="css" scoped></style>
