ae6a67be7605a9cc8865f25f363ee0df42e6cf6b
Elian Remaggi <elianremaggi@gmail.com>
2025-08-28 22:10:27 -0300

    REFACTOR ( Counter Test ) : negative test

--- tests/Counter.nuxt.spec.ts
+++ tests/Counter.nuxt.spec.ts
@@ -3,10 +3,12 @@ import { mountSuspended } from '@nuxt/test-utils/runtime'
 import Counter from '../app/components/counter/Counter.vue'
 
 describe('Counter.vue', () => {
+
     it('incrementa el contador', async () => {
         const wrapper = await mountSuspended(Counter)
         expect(wrapper.text()).toContain('Clicked 0')
         await wrapper.find('button').trigger('click')
         expect(wrapper.text()).toContain('Clicked 1')
     })
+    
 })
